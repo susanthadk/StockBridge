a4149598c8d4fbafb63905af65cf6378ec323816
Susantha Kumara <dksusantha@outlook.com>
2026-08-12 15:58:59 +0530

    removed unused

--- StockBridge.Domain/Entities/SupplierType.cs
+++ StockBridge.Domain/Entities/SupplierType.cs
@@ -1,29 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
+﻿namespace StockBridge.Domain.Entities;
 
-namespace StockBridge.Domain.Entities;
+    public partial class SupplierType
+    {
+        public int SupplierTypeId { get; set; }
 
-public partial class SupplierType
-{
-    public int SupplierTypeId { get; set; }
+        public string SupplierTypeCode { get; set; } = null!;
 
-    public string SupplierTypeCode { get; set; } = null!;
+        public string SupplierTypeName { get; set; } = null!;
 
-    public string SupplierTypeName { get; set; } = null!;
+        public string? Description { get; set; }
 
-    public string? Description { get; set; }
+        public int DisplayOrder { get; set; }
 
-    public int DisplayOrder { get; set; }
+        public bool IsActive { get; set; }
 
-    public bool IsActive { get; set; }
+        public int CreatedBy { get; set; }
 
-    public int CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
 
-    public DateTime CreatedOn { get; set; }
+        public int? ModifiedBy { get; set; }
 
-    public int? ModifiedBy { get; set; }
+        public DateTime? ModifiedOn { get; set; }
 
-    public DateTime? ModifiedOn { get; set; }
-
-    public virtual ICollection<Supplier> Suppliers { get; set; } = new List<Supplier>();
-}
+        public virtual ICollection<Supplier> Suppliers { get; set; } = new List<Supplier>();
+    }
